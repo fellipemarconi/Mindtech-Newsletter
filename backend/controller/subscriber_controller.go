2149dc872d3b822e86014d65ce3dd8d1d718c515
Fellipe Marconi <anksrr@proton.me>
2025-08-22 20:48:10 -0300

    feat: creating controller to create subscriber

---
+++ backend/controller/subscriber_controller.go
@@ -0,0 +1,44 @@
+package controller
+
+import (
+	"api/models"
+	"api/usecase"
+	"errors"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type SubscriberController struct {
+	subscribeUseCase usecase.SubscriberUseCase
+}
+
+func NewSubscriberController(usecase usecase.SubscriberUseCase) SubscriberController {
+	return SubscriberController{
+		subscribeUseCase: usecase,
+	}
+}
+
+func (sc *SubscriberController) CreateSubscriber(ctx *gin.Context) {
+
+	var subscriber models.Subscriber
+	if err := ctx.ShouldBindJSON(&subscriber); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request body"})
+		return
+	}
+
+	insertedSubscriber, err := sc.subscribeUseCase.CreateSubscriber(subscriber)
+	if err != nil {
+		switch {
+		case errors.Is(err, usecase.ErrInvalidEmail):
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": usecase.ErrInvalidEmail.Error()})
+		case errors.Is(err, usecase.ErrEmailExists):
+			ctx.JSON(http.StatusConflict, gin.H{"error": usecase.ErrEmailExists.Error()})
+		default:
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Unexpected error"})
+		}
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, insertedSubscriber)
+}
