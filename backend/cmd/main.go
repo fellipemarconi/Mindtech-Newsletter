7b302454b0b2e4f801466eebec9f8c12a09b330a
Fellipe Marconi <anksrr@proton.me>
2025-08-22 20:49:44 -0300

    chore: adding connection to db
    Connection to db, repository, use case and controller to subscriber and POST route

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,13 +1,47 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"api/controller"
+	"api/db"
+	"api/repository"
+	"api/usecase"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+
+	// Conexão com o banco de dados
+	dbConnection, err := db.ConnectDB()
+	if err != nil {
+		panic(err)
+	}
+
+	// Criação da tabela de subscribers se não existir
+	_, err = dbConnection.Exec(`
+		CREATE TABLE IF NOT EXISTS subscribers 
+		(id SERIAL PRIMARY KEY,
+		email TEXT NOT NULL UNIQUE,
+		created_at TIMESTAMPTZ NOT NULL DEFAULT NOW())
+	`,
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	// Camada de Repositórios
+	productRepository := repository.NewSubscriberRepository(dbConnection)
+	// Camada de UseCases
+	subscriberUseCase := usecase.NewSubscriberUseCase(productRepository)
+	// Camada de Controllers
+	subscriberController := controller.NewSubscriberController(subscriberUseCase)
+
+	// Rotas
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{"message": "Welcome to the Mindtech Newsletter API"})
 	})
-	router.Run() // listen and serve on 0.0.0.0:8080
+	router.POST("/", subscriberController.CreateSubscriber)
+
+	router.Run()
 }
