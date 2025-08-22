dd2580ab1f549143463b4837957f605080317197
Fellipe Marconi <anksrr@proton.me>
2025-08-22 17:22:10 -0300

    feat: creating connection to postgre

---
+++ backend/db/conn.go
@@ -0,0 +1,37 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+func ConnectDB() (*sql.DB, error) {
+	_ = godotenv.Load("../../.env")
+
+	host := os.Getenv("POSTGRES_HOST")
+	port := os.Getenv("POSTGRES_PORT")
+	user := os.Getenv("POSTGRES_USER")
+	password := os.Getenv("POSTGRES_PASSWORD")
+	dbname := os.Getenv("POSTGRES_DB")
+
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Connected to " + dbname)
+
+	return db, nil
+}
