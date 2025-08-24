04453b03fcccfe8a8ecf3f60b21a102d45d772a0
Fellipe Marconi <anksrr@proton.me>
2025-08-24 14:29:01 -0300

    chore: adding connection to frontend files

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -5,20 +5,21 @@ import (
 	"api/db"
 	"api/repository"
 	"api/usecase"
+	"path/filepath"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
 
-	// Conexão com o banco de dados
 	dbConnection, err := db.ConnectDB()
 	if err != nil {
 		panic(err)
 	}
 
-	// Criação da tabela de subscribers se não existir
 	_, err = dbConnection.Exec(`
 		CREATE TABLE IF NOT EXISTS subscribers 
 		(id SERIAL PRIMARY KEY,
@@ -36,13 +37,38 @@ func main() {
 	subscriberUseCase := usecase.NewSubscriberUseCase(productRepository)
 	// Camada de Controllers
 	subscriberController := controller.NewSubscriberController(subscriberUseCase)
+	
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173", "http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: false,
+		MaxAge:           12 * time.Hour,
+	  }))
+	  
+	  const publicDir = "/app/public"
 
-	// Rotas
-	router.GET("/", func(ctx *gin.Context) {
-		ctx.JSON(200, gin.H{"message": "Welcome to the Mindtech Newsletter API"})
-	})
-	router.POST("/", subscriberController.CreateSubscriber)
-	router.DELETE("/", subscriberController.DeleteSubscriber)
-
-	router.Run()
+	  router.Static("/assets", filepath.Join(publicDir, "assets"))
+	  
+	  router.GET("/", func(c *gin.Context) {
+		c.File(filepath.Join(publicDir, "index.html"))
+	  })
+	  
+	  router.GET("/success", func(c *gin.Context) {
+		c.File(filepath.Join(publicDir, "index.html"))
+	  })
+	  
+	  router.NoRoute(func(c *gin.Context) {
+		if c.Request.Method == "GET" {
+		  c.File(filepath.Join(publicDir, "index.html"))
+		  return
+		}
+		c.JSON(404, gin.H{"error": "not found"})
+	  })
+	  
+	  router.POST("/", subscriberController.CreateSubscriber)
+	  router.DELETE("/", subscriberController.DeleteSubscriber)
+	  
+	  router.Run()
 }
